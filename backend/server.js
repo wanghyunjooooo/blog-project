7340f3f2a98462fcf09a3059b01d60ea72f8d4d4
wanghyunjooooo <w2324@e-mirim.hs.kr>
2025-09-18 17:53:00 +0900

    로그인 api 연결

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
 const authRouter = require('./routes/auth');
 
 // 미들웨어
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// CORS 허용 (Go Live에서 테스트할 때 필요)
+app.use(cors());
+
+// 프론트 정적 파일 제공
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 // 라우트 연결
 app.use('/auth', authRouter);
 
 // 기본 라우트
 app.get('/', (req, res) => {
-  res.send('Blog API Server Running!');
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.listen(PORT, () => {
